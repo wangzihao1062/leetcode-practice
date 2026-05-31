33c8e34a5a4e65dd9931e30a1642e614763e50c5
wangzihao <2542511923@qq.com>
2026-05-31 18:26:30 +0800

    feat: add LeetCode twoSum

---
+++ src/com/leetcode/hashmap/twoSum1.java
@@ -0,0 +1,21 @@
+package com.leetcode.hashmap;
+
+import java.util.HashMap;
+
+/*给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+你可以按任意顺序返回答案。*/
+
+public class twoSum1 {
+    public int[] twoSum(int[] nums,  int target){
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if(hm.containsKey(complement)){
+                return new int[]{i, hm.get(complement)};
+            }
+            hm.put(complement, nums[i]);
+        }
+        return new int[0];
+    }
+}

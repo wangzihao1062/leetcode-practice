6eaf96aafcc71472ffe7e37d7c314b99b23e3996
wangzihao <2542511923@qq.com>
2026-06-20 22:51:27 +0800

    feat: add LeetCode 128 longestConsecutive solutions

---
+++ src/com/leetcode/hashmap/LongestConsecutive128.java
@@ -0,0 +1,37 @@
+package com.leetcode.hashmap;
+
+//给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+//输入：nums = [100,4,200,1,3,2]
+//输出：4
+//解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutive128 {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> num_set = new HashSet<Integer>();
+        for (int num : nums) {
+            num_set.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : num_set) {
+            if (!num_set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (num_set.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+}
